7a1a27e9bb0995bf966192dba66b55a7be6b2580
TijmeW <60739363+TijmeWolters@users.noreply.github.com>
2023-10-23 15:42:18 +0200

    added fishshadow

--- src/main/java/com/github/hanyaeger/FishingSimulator/scenes/GameScene.java
+++ src/main/java/com/github/hanyaeger/FishingSimulator/scenes/GameScene.java
@@ -1,5 +1,6 @@
 package com.github.hanyaeger.FishingSimulator.scenes;
 
+import com.github.hanyaeger.FishingSimulator.entities.FishShadow;
 import com.github.hanyaeger.api.Coordinate2D;
 import com.github.hanyaeger.api.scenes.DynamicScene;
 import com.github.hanyaeger.FishingSimulator.entities.Player;
@@ -14,5 +15,6 @@ public class GameScene extends DynamicScene {
     @Override
     public void setupEntities() {
         addEntity(new Player(new Coordinate2D(250, 400)));
+        addEntity(new FishShadow(new Coordinate2D(50, 50)));
     }
 }
